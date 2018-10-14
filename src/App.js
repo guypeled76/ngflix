44a2215b72f29cfff0094e9a4b673b76b1f9b656
Guy Peled <guypeled76@gmail.com>
2018-10-15 01:10:32 +0300

    webview

--- src/App.js
+++ src/App.js
@@ -5,19 +5,39 @@ import Status from './dialogs/Status';
 
 import './App.css';
 
+const { ipcRenderer } = window.require('electron');
+
 class App extends Component {
 
   constructor(props) {
     super(props);
+    this.myRef = React.createRef();
 
+
+  }
+
+  componentDidMount() {
+
+    const webview = this.myRef.current;
+    if (webview) {
+
+      ipcRenderer.on("logon:netflix", function (e, preferences) {
+        alert(1);
+          webview.send("logon:netflix", preferences);
+      });
+
+      ipcRenderer.on("probe:netflix", function (e) {
+          webview.send("probe:netflix");
+      });
+    }
   }
 
   render() {
     const style = { width: '100%', height: '100%' };
-
+    const temp = "file://~/Projects/ElctronJS/ngflix/public/inject.js";
     return (
       <div className="app">
-        <WebView style={style} className="webview" src="https://originator.backlot.netflix.com" preload="./inject.js">
+        <WebView style={style}  ref={this.myRef} className="webview" src="https://originator.backlot.netflix.com" preload={temp}>
         </WebView>
         <Preferences></Preferences>
         <Status></Status>
