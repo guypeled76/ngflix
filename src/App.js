cb8d88435e726d3608142d75fa64bff8b14d5450
Guy Peled <guypeled76@gmail.com>
2018-10-14 06:54:32 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
-import Panel from './Panel';
-import logo from './logo.svg';
+import Preferences from './dialogs/Preferences';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Panel>Test1111</Panel>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Preferences></Preferences>
       </div>
     );
   }
---
+++ src/dialogs/Preferences.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import {
+    Modal,
+    Button,
+    Form,
+    FormGroup,
+    Col,
+    FormControl,
+    ControlLabel,
+    Panel
+} from 'react-bootstrap'
+//import { ipcRenderer } from 'electron';
+
+
+export default class Preferences extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.show = this.show.bind(this);
+        this.close = this.close.bind(this);
+        this.save = this.save.bind(this);
+        this.ngEMailChange = this.ngEMailChange.bind(this);
+        this.ngPasswordChange = this.ngPasswordChange.bind(this);
+        this.userEMailChange = this.userEMailChange.bind(this);
+        this.userPasswordChange = this.userPasswordChange.bind(this);
+        this.notificationEMailChange = this.notificationEMailChange.bind(this);
+
+        this.state = {
+            show: false,
+            ngEMail: '',
+            ngPassword: '',
+            userEMail: '',
+            userPassword: '',
+            notificationEMail: ''
+        };
+
+    }
+
+    /**
+     * Show the preferences dialog
+     * @param {object} preferences 
+     */
+    show(preferences) {
+        this.setState({
+            show: true,
+            ngEMail: preferences.ngEMail,
+            ngPassword: preferences.ngPassword,
+            userEMail: preferences.userEMail,
+            userPassword: preferences.userPassword,
+            notificationEMail: preferences.notificationEMail
+        });
+    }
+
+    /**    
+     * Close the preferences dialog
+     */
+    close() {
+        this.setState({show:false});
+    }
+
+    /**
+     * Save the preferences
+     */
+    save() {
+
+        /*ipcRenderer.send('save:preferences', {
+            ngEMail: this.state.ngEMail,
+            ngPassword: this.state.ngPassword,
+            userEMail: this.state.userEMail,
+            userPassword: this.state.userPassword,
+            notificationEMail: this.state.notificationEMail
+        });*/
+        this.setState({show:false});
+    }
+
+    ngPasswordChange(e) {
+        this.setState({ngPassword:e.target.value});
+    }
+
+    ngEMailChange(e) {
+        this.setState({ngEMail:e.target.value});
+    }
+
+    userEMailChange(e) {
+        this.setState({userEMail:e.target.value});
+    }
+
+    userPasswordChange(e) {
+        this.setState({userPassword:e.target.value});
+    }
+
+    notificationEMailChange(e) {
+        this.setState({notificationEMail:e.target.value});
+    }
+
+    render() {
+        return (<Modal show={this.state.show} onHide={this.close}>
+            <Modal.Header closeButton>
+                <Modal.Title>Preferences</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form horizontal>
+                    <Panel>
+                        <Panel.Heading>NG Notifier</Panel.Heading>
+                        <Panel.Body><FormGroup controlId="formHorizontalEmail">
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Email
+                            </Col>
+                            <Col sm={10}>
+                                <FormControl type="email" placeholder="Email" value={this.state.ngEMail} onChange={this.ngEMailChange} />
+                            </Col>
+                        </FormGroup>
+
+                            <FormGroup controlId="formHorizontalPassword">
+                                <Col componentClass={ControlLabel} sm={2}>
+                                    Password
+                            </Col>
+                                <Col sm={10}>
+                                    <FormControl type="password" placeholder="Password" value={this.state.ngPassword} onChange={this.ngPasswordChange} />
+                                </Col>
+                            </FormGroup></Panel.Body>
+                    </Panel>
+
+                    <Panel>
+                        <Panel.Heading>User</Panel.Heading>
+                        <Panel.Body><FormGroup controlId="formHorizontalEmail">
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Email
+                            </Col>
+                            <Col sm={10}>
+                                <FormControl type="email" placeholder="Email" value={this.state.userEMail} onChange={this.userEMailChange} />
+                            </Col>
+                        </FormGroup>
+
+                            <FormGroup controlId="formHorizontalPassword">
+                                <Col componentClass={ControlLabel} sm={2}>
+                                    Password
+                                </Col>
+                                <Col sm={10}>
+                                    <FormControl type="password" placeholder="Password" value={this.state.userPassword} onChange={this.userPasswordChange} />
+                                </Col>
+                            </FormGroup></Panel.Body>
+                    </Panel>
+                    
+                    <Panel>
+                        <Panel.Heading>Notifications</Panel.Heading>
+                        <Panel.Body>
+                            <FormGroup controlId="formHorizontalEmail">
+                                <Col componentClass={ControlLabel} sm={2}>
+                                    Email
+                                </Col>
+                                <Col sm={10}>
+                                    <FormControl type="email" placeholder="Email" value={this.state.notificationEMail} onChange={this.notificationEMailChange} />
+                                </Col>
+                            </FormGroup>
+                        </Panel.Body>
+                    </Panel>
+
+                </Form>
+
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={this.close}>Close</Button>
+                <Button onClick={this.save}>Save</Button>
+            </Modal.Footer>
+        </Modal>);
+    }
+}
